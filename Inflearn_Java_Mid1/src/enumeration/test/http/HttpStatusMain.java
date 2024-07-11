5620d4cadbe3ed4cf293d70db36f91d2c0b9f30f
Yjunha <junay00v@gmail.com>
2024-07-12 02:29:15 +0900

    Section 5. 열거형 - ENUM 문제와 풀이2

--- Inflearn_Java_Mid1/src/enumeration/test/http/HttpStatusMain.java
+++ Inflearn_Java_Mid1/src/enumeration/test/http/HttpStatusMain.java
@@ -10,7 +10,8 @@ public class HttpStatusMain {
         int httpCodeInput = scanner.nextInt();
         HttpStatus status = HttpStatus.findByCode(httpCodeInput);
         if (status == null) {
-            System.out.println("정의되지 않은 코드"); } else {
+            System.out.println("정의되지 않은 코드"); }
+        else {
             System.out.println(status.getCode() + " " + status.getMessage());
             System.out.println("isSuccess = " + status.isSuccess());
         }
