178a165a36e58ed1f76d05438c2ed168e4266829
Yjunha <junay00v@gmail.com>
2024-07-11 18:12:41 +0900

    Section 5. 열거형 - ENUM 문자열과 타입 안정성2

---
+++ Inflearn_Java_Mid1/src/enumeration/ex0/StringGradeEx0_1.java
@@ -0,0 +1,17 @@
+package enumeration.ex0;
+
+public class StringGradeEx0_1 {
+
+    public static void main(String[] args) {
+        int pirce = 10000;
+
+        DiscountService discountService = new DiscountService();
+        int basic = discountService.discount("BASIC",pirce);
+        int gold = discountService.discount("GOLD",pirce);
+        int diamond = discountService.discount("DIAMOND",pirce);
+
+        System.out.println("BASIC 등급의 할인 금액: " + basic);
+        System.out.println("GOLD 등급의 할인 금액: " + gold);
+        System.out.println("DIAMOND 등급의 할인 금액: " + diamond);
+    }
+}
