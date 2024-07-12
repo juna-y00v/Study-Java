5db46410aed9e935072e0d3b05bee2bee8609084
Yjunha <junay00v@gmail.com>
2024-07-12 18:11:18 +0900

    Section 7. 중첩 클래스, 내부 클래스1 내부 클래스의 활용

--- Inflearn_Java_Mid1/src/nested/inner/ex1/CarMain.java
+++ Inflearn_Java_Mid1/src/nested/inner/ex1/CarMain.java
@@ -3,6 +3,6 @@ package nested.inner.ex1;
 public class CarMain {
     public static void main(String[] args) {
         Car myCar = new Car("Model Y", 100);
-        myCar.start();g
+        myCar.start();
     }
 }
